1b16c6d504142606256bdb1ad84f349e7816da73
therandomfool <mddenver@protonmail.com>
2020-05-15 16:37:53 -0700

    Stub Boot/MVC Project connect JPA project dependency

---
+++ KnivesApp/src/main/java/com/skilldistillery/japanese_knives/KnivesAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.japanese_knives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnivesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnivesAppApplication.class, args);
+	}
+
+}
